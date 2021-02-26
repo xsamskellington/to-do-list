43e6bcf03418c75113136f7d6b8aedfa03464444
xsamskellington <samantha.valdez.toccalino@gmail.com>
2021-02-25 18:24:51 -0600

    adding local storage

--- src/App.js
+++ src/App.js
@@ -10,7 +10,12 @@ function App() {
   const [filteredTodos, setFilteredTodos] = useState([]);
 
   useEffect(()=>{
+    getLocalTodos()
+  },[])
+
+  useEffect(()=>{
     filterHandler()
+    saveLocalTodos()
   }, [todos, status])
   
   const filterHandler =() =>{
@@ -26,6 +31,19 @@ function App() {
         break;
     }
   }
+
+  const saveLocalTodos = () => {
+      localStorage.setItem('todos', JSON.stringify(todos))
+    }
+
+  const getLocalTodos = () => {
+    if(localStorage.getItem('todos') === null){
+      localStorage.setItem('todos', JSON.stringify([]));
+    }else{
+      let todoLocal = JSON.parse(localStorage.getItem('todos'))
+      setTodos(todoLocal);
+    }
+    }
 
 
 
